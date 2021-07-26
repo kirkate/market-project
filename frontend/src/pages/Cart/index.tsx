3086c7bc2f96e4b894ef0d0dbf1b509804c37346
kkir <miculishna13>
2021-07-26 17:19:06 +0300

    purchase button and total price

--- frontend/src/pages/Cart/index.tsx
+++ frontend/src/pages/Cart/index.tsx
@@ -4,53 +4,80 @@ import { CartService } from "../../services/cartService";
 
 export const Cart = () => {
   const [cart, setCart] = useState(CartService.getItems());
+  const [quantityProducts, setQuantityProducts] = useState(1);
+  const [totalPrice, setTotalPrice] = useState(0);
 
   useEffect(() => {
     CartService.watch(setCart);
   }, []);
 
+  useEffect(() => {
+    setTotalPrice(
+      cart.reduce((sum, i) => {
+        return sum + i.price * (i.quantity || 1);
+      }, 0)
+    );
+  }, [cart.length, quantityProducts]);
+
   const handleDeleteProduct = (id) => {
     CartService.deleteItem(id);
   };
-  const handleClear = () => {
+  const handleClearCart = () => {
     CartService.clearCart();
   };
   const handleChangeQuantity = (id, { target: { value } }) => {
     CartService.updateItemsQuantity(id, value);
+    setQuantityProducts(Number(value));
+  };
+  const handleSubmitPurchases = (event) => {
+    event.preventDefault();
   };
 
   return (
     <section>
       <p>Cart ({cart.length})</p>
-      <If condition={cart}>
-        <ul class="cartList">
-          <For each="cartItem" of={cart}>
-            <li key={index} class="cartItem">
-              <p>{cartItem.title}</p>
-              <input
-                id="count"
-                type="number"
-                class="cart-form__input"
-                value={cartItem.quantity || 1}
-                min={1}
-                max={100}
-                onChange={() => handleChangeQuantity(cartItem.id, event)}
-              />
-              <button
-                type="button"
-                onClick={() => handleDeleteProduct(cartItem.id)}
-              >
-                delete
-              </button>
-            </li>
-          </For>
-        </ul>
-      </If>
-      {cart && cart.length > 0 && (
-        <button type="button" onClick={handleClear}>
-          Clear all
-        </button>
-      )}
+      <form onSubmit={handleSubmitPurchases}>
+        <If condition={cart}>
+          <ul class="cartList">
+            <For each="cartItem" of={cart}>
+              <li key={index} class="cartItem">
+                <p>{cartItem.title}</p>
+                <input
+                  id="count"
+                  type="number"
+                  class="cart-form__input"
+                  value={cartItem.quantity || 1}
+                  min={1}
+                  max={100}
+                  onChange={() => handleChangeQuantity(cartItem.id, event)}
+                />
+                <span>
+                  {cartItem.quantity
+                    ? cartItem.price * cartItem.quantity
+                    : cartItem.price}{" "}
+                  $
+                </span>
+                <button
+                  type="button"
+                  onClick={() => handleDeleteProduct(cartItem.id)}
+                >
+                  delete
+                </button>
+              </li>
+            </For>
+          </ul>
+        </If>
+        {cart && cart.length > 0 && (
+          <button type="button" onClick={handleClearCart}>
+            Clear all
+          </button>
+        )}
+        <div>
+          <p>Total price</p>
+          <span>${totalPrice}</span>
+        </div>
+        <button type="submit">Purchase</button>
+      </form>
     </section>
   );
 };
