4774741992efe2f338b4ea92e00908cda8aa2c74
kkir <miculishna13>
2021-09-06 13:24:49 +0300

    style

--- frontend/src/components/CartTable/index.tsx
+++ frontend/src/components/CartTable/index.tsx
@@ -2,55 +2,47 @@ import { h } from 'preact';
 import { Button } from '../Button';
 
 export const CartTable = ({ cart, onDelete, onQuantityChange }) => (
-  <form class="cart-form">
+  <form class='cart-form'>
     <If condition={cart.positions}>
       <table class="cart-form__list">
         <tbody>
-          <For each="position" of={cart.positions}>
-
-            <tr>
-              <td>
-                {' '}
-                <p>{position.product.title}</p>
+        <For each="position" of={cart.positions}>
+          <tr>
+            <td>
+              <p>{position.product.title}</p></td>
+            <td>
+            <fieldset>
+            <input
+              name="productsQuantity"
+              type="number"
+              class="cart-list__input"
+              value={position.quantity || 1}
+              min={1}
+              max={100}
+              onChange={() => onQuantityChange(position.product.id, event.target.value)}
+            />
+            </fieldset>
               </td>
-
-              <td>
-                <fieldset>
-                  <input
-                    name="productsQuantity"
-                    type="number"
-                    class="cart-list__input"
-                    value={position.quantity || 1}
-                    min={1}
-                    max={100}
-                    onChange={() => onQuantityChange(position.product.id, event.target.value)}
-                  />
-
-                </fieldset>
+            <td>
+            <span>
+              {position.total}
+              $
+            </span>
               </td>
-
-              <td>
-                <span>
-                  {position.total}
-                  $
-                </span>
+            <td>
+            <Button
+              type="button"
+              onClick={() => onDelete(position.product.id)}
+            >
+              delete
+            </Button>
               </td>
-
-              <td>
-                <Button
-                  type="button"
-                  onClick={() => onDelete(position.product.id)}
-                >
-                  delete
-                </Button>
-              </td>
-            </tr>
-
-          </For>
+          </tr>
+        </For>
         </tbody>
       </table>
       <If condition={cart.positions?.length > 0}>
-        <p class="cart-form__price">
+        <p class='cart-form__price'>
           Total price $
           {cart.total}
         </p>
